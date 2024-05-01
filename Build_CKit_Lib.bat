6469f76ad17772ba8c36c3559cc28a5c632324bc
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-05-01 17:41:04 -0400

    Update Build_CKit_Lib.bat

--- Build_CKit_Lib.bat
+++ Build_CKit_Lib.bat
@@ -15,7 +15,7 @@ if exist compilation_errors.txt (
 pushd .\build
 :: MAKE SURE YOU HAVE AN OPTION FOR DEBUG LIBS
 :: cl -DCUSTOM_PLATFORM_IMPL /std:c++20 /c "..\source\*.cpp"
-cl /std:c++20 /Zi /FC /c "..\source\*.cpp" >> "..\compilation_errors.txt" 2>nul
+cl /Zi /std:c++20 /FC /c "..\source\*.cpp" >> "..\compilation_errors.txt" 2>nul
 lib /OUT:".\CKit.lib" ".\*.obj" > NUL
 popd
 
