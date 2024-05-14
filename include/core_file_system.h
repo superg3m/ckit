efa1f5045360307bfd1258ebc8bfbb8768289606
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-05-14 18:38:39 -0400

    file system nice!

---
+++ include/core_file_system.h
@@ -0,0 +1,29 @@
+#pragma once
+/*===========================================================
+ * File: core_file_system.h
+ * Date: May 14, 2024
+ * Creator: Jovanni Djonaj
+===========================================================*/
+#include <stdio.h>
+#include "core_assert.h"
+
+typedef struct FileSystem {
+	Boolean reachedEOF;
+	const char* fileName;
+	FILE* file;
+} FileSystem;
+
+//************************* Begin Functions *************************
+#ifdef __cplusplus
+extern "C" {
+#endif
+	FileSystem file_system_create(const char* fileName);
+	void file_open(FileSystem* file_system);
+	void file_close(FileSystem* file_system);
+	String file_get_next_line(FileSystem* file_system);
+	char file_get_next_char(FileSystem* file_system);
+#ifdef __cplusplus
+}
+#endif
+//************************** End Functions **************************
+
---
+++ source/core_file_system.c
@@ -0,0 +1,38 @@
+#include "../include/core_file_system.h"
+#include "../include/core_string.h"
+
+void file_open(FileSystem* fileFramework) {
+  fileFramework->file = fopen(fileFramework->fileName, "r");
+  assert_in_function(fileFramework->file != NULL, "FILE IS NULL, CHECK INITIAL FILE NAME");
+}
+
+String file_get_next_line(FileSystem* fileFramework) {
+  String line = string_create("");
+  char c;
+  do {
+    c = fgetc(fileFramework->file);
+    if (c != '\n' && c != EOF) {
+      string_append_char(line, c);
+    }
+    if (c == EOF) {
+      fileFramework->reachedEOF = TRUE;
+    }
+  } while (c != '\n' && c != EOF);
+  return line;
+}
+
+char file_get_next_char(FileSystem* fileFramework) {
+  return fgetc(fileFramework->file);
+}
+
+void file_close(FileSystem* fileFramework) { 
+    fclose(fileFramework->file); 
+}
+
+FileSystem createFileFramework(const char* fileName) {
+  FileSystem file_system;
+  file_system.file = NULL;
+  file_system.reachedEOF = FALSE;
+  file_system.fileName = fileName;
+  return file_system;
+}
