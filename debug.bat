dbacb7a5db0e7bfecd71b459fa7e71e1e45af30d
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-05-23 15:00:24 -0400

    Update debug.bat

--- debug.bat
+++ debug.bat
@@ -1,7 +1,7 @@
 @echo off
 
-if not exist .\examples\CKit_Test.sln (
-    if not exist .\examples\CKit_Test.exe (
+if not exist .\examples\cl\CKit_Test.sln (
+    if not exist .\examples\cl\CKit_Test.exe (
         powershell -nologo -command "Write-Host 'ERROR: check if exe exists and re-build' -ForegroundColor Red"
     ) else (
         devenv .\examples\cl\CKit_Test.exe
