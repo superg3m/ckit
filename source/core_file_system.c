d2ea7b23a54a10d297bcac6d45b1702f2ce7e1c8
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-05-26 11:26:38 -0400

    Update core_file_system.c

--- source/core_file_system.c
+++ source/core_file_system.c
@@ -2,27 +2,27 @@
 #include "../include/core_string.h"
 
 void file_open(FileSystem* fileFramework) {
-  fileFramework->file = fopen(fileFramework->fileName, "r");
-  assert_in_function(fileFramework->file != NULL, "FILE IS NULL, CHECK INITIAL FILE NAME\n");
+	fileFramework->file = fopen(fileFramework->fileName, "r");
+	assert_in_function(fileFramework->file != NULL, "FILE IS NULL, CHECK INITIAL FILE NAME\n");
 }
 
 String file_get_next_line(FileSystem* fileFramework) {
-  String line = string_create("");
-  char c;
-  do {
-    c = fgetc(fileFramework->file);
-    if (c != '\n' && c != EOF) {
-      string_append_char(line, c);
-    }
-    if (c == EOF) {
-      fileFramework->reachedEOF = TRUE;
-    }
-  } while (c != '\n' && c != EOF);
-  return line;
+	String line = string_create("");
+	char c;
+	do {
+		c = fgetc(fileFramework->file);
+		if (c != '\n' && c != EOF) {
+		string_append_char(line, c);
+		}
+		if (c == EOF) {
+		fileFramework->reachedEOF = TRUE;
+		}
+	} while (c != '\n' && c != EOF);
+	return line;
 }
 
 char file_get_next_char(FileSystem* fileFramework) {
-  return fgetc(fileFramework->file);
+    return fgetc(fileFramework->file);
 }
 
 void file_close(FileSystem* fileFramework) { 
@@ -30,9 +30,9 @@ void file_close(FileSystem* fileFramework) {
 }
 
 FileSystem file_system_create(const char* fileName) {
-  FileSystem file_system;
-  file_system.file = NULL;
-  file_system.reachedEOF = FALSE;
-  file_system.fileName = fileName;
-  return file_system;
+	FileSystem file_system;
+	file_system.file = NULL;
+	file_system.reachedEOF = FALSE;
+	file_system.fileName = fileName;
+	return file_system;
 }
