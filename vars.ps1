544e4ef7ba7b99ed8a9fa603d9f047ccb7f89685
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-06-07 17:43:33 -0400

    good

--- vars.ps1
+++ vars.ps1
@@ -1,19 +1,22 @@
 # Define the path to the vcvarsall.bat script
-$clAvailable =  $null -ne (Get-Command cl -ErrorAction SilentlyContinue) 
+$ErrorActionPreference = "Stop"
+Set-StrictMode -Version Latest
+$PSNativeCommandUseErrorActionPreference = $true
+
+$clAvailable = $null -ne (Get-Command cl -ErrorAction SilentlyContinue)
 
 if (!$clAvailable) {
-	$vcvarsallPath = "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat"
+    $vcvarsallPath = "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat"
 
-	# Run the batch file and capture the environment variables it sets
-	$envVars = & cmd /c "call `"$vcvarsallPath`" x64 > throw_null.dev && set"
+    # Run the batch file and capture the environment variables it sets
+    $envVars = & cmd /c "call `"$vcvarsallPath`" x64 > NUL && set"
 
-	Remove-Item -Path ./"throw_null.dev"
-	
-	# Import the environment variables into PowerShell
-	$envVars -split "`r`n" | ForEach-Object {
-		$name, $value = $_ -split '=', 2
-		[Environment]::SetEnvironmentVariable($name, $value, [System.EnvironmentVariableTarget]::Process)
-	}
-	
-	Write-Host "Visual Studio environment variables have been set." -ForegroundColor Green
+    # Import the environment variables into PowerShell
+    $envVars -split "`r`n" | ForEach-Object {
+        $name, $value = $_ -split '=', 2
+        [Environment]::SetEnvironmentVariable($name, $value, [System.EnvironmentVariableTarget]::Process)
+    }
+
+    Write-Host "Visual Studio environment variables have been set." -ForegroundColor Green
 }
+
