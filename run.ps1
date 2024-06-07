2727033c98c708944af766dca08bb1561df3b638
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-06-07 18:34:12 -0400

    f

--- run.ps1
+++ run.ps1
@@ -3,19 +3,12 @@ $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
 $PSNativeCommandUseErrorActionPreference = $true
 
-$configPath = "config.json"
+$configPath = "c_build_config.json"
 $jsonData = Get-Content -Path $configPath -Raw | ConvertFrom-Json
 
-$lib_name = $jsonData.'$lib_name'
-$output_name = $jsonData.'$executable_name'
-$compile_time_defines = $jsonData.'$compile_time_defines'
-$std_version = $jsonData.'$std_version'
-$debug_build = $jsonData.'$debug_build'
-$build_lib = $jsonData.'$build_lib'
-$generate_object_files = $jsonData.'$generate_object_files'
-$include_paths = $jsonData.'$include_paths'
-$source_paths = $jsonData.'$source_paths'
-$additional_libs_for_build = $jsonData.'$additional_libs_for_build'
+$project_name = $jsonData.'$project_name'
+
+Write-Host "running [$project_name] run.ps1..." -ForegroundColor Green
 
 Push-Location  ".\C-BUILD"
 git stash
@@ -23,8 +16,7 @@ git stash drop
 git pull
 Pop-Location
 
-./build.ps1
-& "./"
+./build.ps1 -run_exe $true
 
 # run is complicated because what do you want to run?
 
