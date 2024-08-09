80dd487574431fb2e6d34006454eafb3e74cfbea
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-08-09 18:48:16 -0400

    ok

--- ckit.h
+++ ckit.h
@@ -6,28 +6,28 @@
 ===========================================================*/
 
 // CORE CKIT
-#include "ckit_types.h"
-#include "ckit_logger.h"
-#include "ckit_memory.h"
-#include "ckit_arena.h"
-#include "ckit_memory_tracker.h"
-#include "ckit_assert.h"
-#include "ckit_event_system.h"
-#include "ckit_string.h"
-#include "ckit_char.h"
-#include "ckit_file_system.h"
-#include "ckit_math.h"
-#include "ckit_vector.h"
-#include "ckit_hashing.h"
-#include "ckit_hashmap.h"
-#include "ckit_hashset.h"
-#include "ckit_linked_list.h"
-#include "ckit_queue.h"
-#include "ckit_stack.h"
+#include "./Include/ckit_types.h"
+#include "./Include/ckit_logger.h"
+#include "./Include/ckit_memory.h"
+#include "./Include/ckit_arena.h"
+#include "./Include/ckit_memory_tracker.h"
+#include "./Include/ckit_assert.h"
+#include "./Include/ckit_event_system.h"
+#include "./Include/ckit_string.h"
+#include "./Include/ckit_char.h"
+#include "./Include/ckit_file_system.h"
+#include "./Include/ckit_math.h"
+#include "./Include/ckit_vector.h"
+#include "./Include/ckit_hashing.h"
+#include "./Include/ckit_hashmap.h"
+#include "./Include/ckit_hashset.h"
+#include "./Include/ckit_linked_list.h"
+#include "./Include/ckit_queue.h"
+#include "./Include/ckit_stack.h"
 
 // Modules
-#include "./bmp_parser.h"
-#include "./ckit_lexer.h"
+#include "./Include/bmp_parser.h"
+#include "./Include/ckit_lexer.h"
 
 
 #ifdef __cplusplus
