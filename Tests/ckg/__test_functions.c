f64b61f21577d1434e15e790011e3e954fa0bf3e
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-08-11 14:02:59 -0400

    removing ckg completely

--- Tests/ckg/__test_functions.c
+++
@@ -1,6 +0,0 @@
-#include "../../ckg.h"
-
-#include "./__test_memory_functions.c"
-#include "./__test_arena_functions.c"
-#include "./__test_vector_functions.c"
-#include "./__test_string_functions.c"
