e609ec8221cba0fb7f7a5ebd4df567f58ef4604e
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-08-15 16:50:59 -0400

    Little progress at a time

--- Include/Modules/Graphics/ckit_opengl.h
+++ Include/Modules/Graphics/ckit_opengl.h
@@ -1,5 +1,8 @@
-#include "../../Core/Basic/ckit_types.h"
-#include <glad/glad.h>
+#include "../../../Core/Basic/ckit_types.h"
+#include "../../../Core/Basic/ckit_logger.h"
+#include "../../../Core/Collections/ckit_vector.h"
+#include "./glad/glad.h"
+#include "./stb_image.h"
 
 // Textures
 
@@ -11,6 +14,34 @@ typedef struct CKIT_Shader {
 
 CKIT_Shader ckit_shader_create() {
 
+}
+
+CKIT_Shader ckit_shader_add_texture(CKIT_Shader* shader) {
+  u32 texture;
+  glGenTextures(1, &texture);
+  glBindTexture(GL_TEXTURE_2D, texture);
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  int width, height, nrChannels;
+  u8 *data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
+  if (data) {
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+  } else {
+    LOG_ERROR("Failed to load texture\n");
+  }
+  stbi_image_free(data);  
+}
+
+CKIT_Shader ckit_shader_bind_texture(CKIT_Shader* shader) {
+  for (u32 i = 0; i < ckit_vector_count(shader->textures); i++) {
+    glActiveTexture(GL_TEXTURE0 + i);
+    glBindTexture(GL_TEXTURE_2D, shader->textures[i]);
+  }
 }
 
 void ckit_shader_use(CKIT_Shader* shader) {
