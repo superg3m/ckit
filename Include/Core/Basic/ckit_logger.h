f18ad9bcceef2162bb67980a38679edd7c7ba5aa
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-08-15 11:47:06 -0400

    Update ckit_logger.h

--- Include/Core/Basic/ckit_logger.h
+++ Include/Core/Basic/ckit_logger.h
@@ -154,9 +154,10 @@ extern "C" {
 
         if (message_has_special_delmitor(out_message)) {
             special_print_helper(out_message, log_level);
-        } else {
-            printf("%s%.*s%s", log_level_format[log_level], out_message_length - 1, out_message, CKG_COLOR_RESET);
-        }
+        } else if(out_message[out_message_length - 1] == '\n') {
+            Boolean found = out_message[out_message_length - 1] == '\n';
+            printf("%s%.*s%s", log_level_format[log_level], out_message_length - found, out_message, CKG_COLOR_RESET);
+        } 
 
         if (out_message[out_message_length - 1] == '\n') {
             printf("\n");
