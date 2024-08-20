ae55158be814d4a2a8e69a1816abd8d1554ad040
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-08-20 09:58:22 -0400

    Update ckit_native_window.h

--- Include/Modules/Graphics/ckit_native_window.h
+++ Include/Modules/Graphics/ckit_native_window.h
@@ -252,14 +252,8 @@ extern "C" {
 
 			for (u32 y = 0; y < true_quad_height; y++) {
 				for (u32 x = 0; x < true_quad_width; x++) {
-					const u32 red = ((color.r) << 16);
-					const u32 green = ((color.g) << 8);
-					const u32 blue = ((color.b) << 0);
-					
-					const u32 rgb = red|green|blue;
-
 					size_t final_pixel_index = x + (y * VIEWPORT_WIDTH);
-					dest[final_pixel_index] = rgb;
+					dest[final_pixel_index] = ckit_color_to_u32(color);
 				}
 			}
 		}
@@ -492,7 +486,7 @@ extern "C" {
 		Boolean ckit_window_should_quit(CKIT_Window* window) {
 			XNextEvent(display, &event);
 			if (event.type == Expose) {
-				XPutImage(display, win, gc, ximage, 0, 0, 0, 0, width, height);
+				XPutImage(window->x11_display, window->x11_window, window->bitmap->x11_gc, window->bitmap->x11_ximage, 0, 0, 0, 0, window->bitmap->x11_width, window->bitmap->x11_height);
 			}
 		}
 
@@ -500,13 +494,34 @@ extern "C" {
 			// Write to the bitmap
 			for (int y = 0; y < window->memory->bitmap->height; ++y) {
 				for (int x = 0; x < window->memory->bitmap->width; ++x) {
-					XPutPixel(bitmap->memory, x, y, ckit_color_to_u32(color));  // Simple pattern
+					XPutPixel(bitmap->memory, x, y, ckit_color_to_u32(color));  // might have to endian swap the color
 				}
 			}
 		}
 
 		void ckit_window_draw_quad(CKIT_Window* window, s32 start_x, s32 start_y, u32 width, u32 height, CKIT_Color color) {
-			
+			const u32 VIEWPORT_WIDTH = window->bitmap->width;
+			const u32 VIEWPORT_HEIGHT = window->bitmap->height;
+
+			u32 left = CLAMP(start_x, 0, VIEWPORT_WIDTH);
+			u32 right = CLAMP(start_x + width, 0, VIEWPORT_WIDTH);
+
+			u32 bottom = CLAMP(start_y, 0, VIEWPORT_HEIGHT);
+			u32 top = CLAMP(start_y + height, 0, VIEWPORT_HEIGHT);
+
+			u32 true_quad_width = right - left;
+			u32 true_quad_height = top - bottom;
+
+			Boolean should_draw = (true_quad_width != 0) && (true_quad_height != 0);
+			if (!should_draw) {
+				return;
+			}
+
+			for (u32 y = 0; y < true_quad_height; y++) {
+				for (u32 x = 0; x < true_quad_width; x++) {
+					XPutPixel(bitmap->memory, x, y, ckit_color_to_u32(color));
+				}
+			}
 		}
 
 		void ckit_window_draw_bitmap(CKIT_Window* window) {
