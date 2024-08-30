9d023649009633c3e79086bf5266564ea0b3a489
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-08-29 21:04:18 -0400

    big ideas

---
+++ Include/Modules/Graphics/ckit_graphics_imgui.h
@@ -0,0 +1,9 @@
+// button 
+// info tooltip
+// resizable panel
+// grouping together components
+	// grouped components should have a relative and absolute location. Absolute is the panel location and relative is where in the panel relative to the panel stuff is.
+// slider
+// button toggle
+// multi-checkbox
+// dropdown 
