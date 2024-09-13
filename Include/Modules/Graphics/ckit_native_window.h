815ae48e660d7fa430c77b3feb1dbe70b6bd2f90
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-09-13 11:20:27 -0400

    Update ckit_native_window.h

--- Include/Modules/Graphics/ckit_native_window.h
+++ Include/Modules/Graphics/ckit_native_window.h
@@ -109,6 +109,10 @@ typedef enum CKIT_CursorState {
 		return 0;
 	}
 	*/
+#else
+	typedef struct CKIT_Window {
+		int a;
+	} CKIT_Window;
 #endif
 //=========================== End Types ===========================
 
@@ -737,6 +741,61 @@ extern "C" {
 
 		void ckit_window_swap_buffers(CKIT_Window* window) {
 			
+		}
+
+	#else
+		CKIT_Window* ckit_window_create(u32 width, u32 height, const char* name) {
+			ckit_assert_msg(FALSE, "NOT IMPLEMENTED YET!");	
+			return NULLPTR;
+		}
+
+		void* MACRO_ckit_window_free(CKIT_Window* window) {
+			ckit_assert_msg(FALSE, "NOT IMPLEMENTED YET!");	
+		}
+
+		void ckit_window_bind_icon(const char* resource_path) {
+			ckit_assert_msg(FALSE, "NOT IMPLEMENTED YET!");	
+		}
+
+		void ckit_window_bind_cursor(const char* resource_path) {
+			ckit_assert_msg(FALSE, "NOT IMPLEMENTED YET!");	
+		}
+
+		Boolean ckit_window_should_quit(CKIT_Window* window) {
+			ckit_assert_msg(FALSE, "NOT IMPLEMENTED YET!");	
+			return FALSE;
+		}
+
+		void ckit_window_clear_color(CKIT_Window* window, CKIT_Color color) {
+			ckit_assert_msg(FALSE, "NOT IMPLEMENTED YET!");	
+		}
+
+		void ckit_window_draw_quad(CKIT_Window* window, CKIT_Rectangle2D rectangle, CKIT_Color color) {
+			ckit_assert_msg(FALSE, "NOT IMPLEMENTED YET!");	
+		}
+
+		void ckit_window_draw_line(CKIT_Window* window, CKIT_Vector3 p1, CKIT_Vector3 p2) {
+			ckit_assert_msg(FALSE, "NOT IMPLEMENTED YET!");	
+		}
+
+		void ckit_window_draw_circle(CKIT_Window* window, s32 start_x, s32 start_y, s32 radius, Boolean is_filled, CKIT_Color color) {
+			ckit_assert_msg(FALSE, "NOT IMPLEMENTED YET!");	
+		}
+
+		void ckit_window_draw_bitmap(CKIT_Window* window, s32 start_x, s32 start_y, u32 scale_factor, CKIT_Bitmap bitmap) {
+			ckit_assert_msg(FALSE, "NOT IMPLEMENTED YET!");	
+		}
+
+		void ckit_window_swap_buffers(CKIT_Window* window) {
+			ckit_assert_msg(FALSE, "NOT IMPLEMENTED YET!");	
+		}
+
+		void ckit_window_get_mouse_position(CKIT_Window* window, s32* mouse_x, s32* mouse_y) {
+			ckit_assert_msg(FALSE, "NOT IMPLEMENTED YET!");	
+		}
+
+		void ckit_window_set_cursor_state(CKIT_Window* window, CKIT_CursorState cursor_state) {
+			ckit_assert_msg(FALSE, "NOT IMPLEMENTED YET!");	
 		}
 	#endif
 #endif // CKG_IMPL
