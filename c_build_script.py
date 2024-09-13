89eac01e9cd250957b83e78365422263dc4f0d01
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-09-13 10:37:17 -0400

    Update c_build_script.py

--- c_build_script.py
+++ c_build_script.py
@@ -26,7 +26,7 @@ if COMPILER == "cl":
 	project.set_compiler_warning_level("1")
 	project.disable_specific_warnings(["5105", "4668", "4820"])
 elif COMPILER in ["gcc", "cc", "clang"]:
-	project.disable_specific_warnings(["deprecated", "pointer-sign"])
+	project.disable_specific_warnings(["deprecated", "pointer-sign", "parentheses"])
 
 project.set_treat_warnings_as_errors(True)
 project.set_debug_with_visual_studio(True)
