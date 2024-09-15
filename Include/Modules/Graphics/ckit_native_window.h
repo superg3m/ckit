c41fc7ed22649ad3cc07fbe57a67aa5d3ee87d2d
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-09-15 17:29:58 -0400

    Update ckit_native_window.h

--- Include/Modules/Graphics/ckit_native_window.h
+++ Include/Modules/Graphics/ckit_native_window.h
@@ -608,35 +608,57 @@ extern "C" {
 
 	#elif defined(CKIT_WSL)
 		CKIT_Window* ckit_window_create(u32 width, u32 height, const char* name) {
-
+			ckit_assert_msg(FALSE, "NOT IMPLEMENTED YET!");
+			return NULLPTR;
 		}
 
 		void* MACRO_ckit_window_free(CKIT_Window* window) {
-
+			ckit_assert_msg(FALSE, "NOT IMPLEMENTED YET!");
 		}
 
 		void ckit_window_bind_icon(const char* resource_path) {
-
+			ckit_assert_msg(FALSE, "NOT IMPLEMENTED YET!");
 		}
 
 		void ckit_window_bind_cursor(const char* resource_path) {
-
+			ckit_assert_msg(FALSE, "NOT IMPLEMENTED YET!");
 		}
 
 		Boolean ckit_window_should_quit(CKIT_Window* window) {
-
+			ckit_assert_msg(FALSE, "NOT IMPLEMENTED YET!");
+			return FALSE;
 		}
 
 		void ckit_window_clear_color(CKIT_Window* window, CKIT_Color color) {
-
+			ckit_assert_msg(FALSE, "NOT IMPLEMENTED YET!");
 		}
 
-		void ckit_window_draw_quad(CKIT_Window* window, s32 start_x, s32 start_y, u32 width, u32 height, CKIT_Color color) {
-
+		void ckit_window_draw_quad(CKIT_Window* window, CKIT_Rectangle2D rectangle, CKIT_Color color) {
+			ckit_assert_msg(FALSE, "NOT IMPLEMENTED YET!");
 		}
 
-		void ckit_window_draw_bitmap(CKIT_Window* window) {
+		void ckit_window_draw_line(CKIT_Window* window, CKIT_Vector3 p1, CKIT_Vector3 p2) {
+			ckit_assert_msg(FALSE, "NOT IMPLEMENTED YET!");
+		}
 
+		void ckit_window_draw_circle(CKIT_Window* window, s32 start_x, s32 start_y, s32 radius, Boolean is_filled, CKIT_Color color) {
+			ckit_assert_msg(FALSE, "NOT IMPLEMENTED YET!");
+		}
+
+		void ckit_window_draw_bitmap(CKIT_Window* window, s32 start_x, s32 start_y, u32 scale_factor, CKIT_Bitmap bitmap) {
+			ckit_assert_msg(FALSE, "NOT IMPLEMENTED YET!");
+		}
+
+		void ckit_window_swap_buffers(CKIT_Window* window) {
+			ckit_assert_msg(FALSE, "NOT IMPLEMENTED YET!");
+		}
+
+		void ckit_window_get_mouse_position(CKIT_Window* window, s32* mouse_x, s32* mouse_y) {
+			ckit_assert_msg(FALSE, "NOT IMPLEMENTED YET!");
+		}
+
+		void ckit_window_set_cursor_state(CKIT_Window* window, CKIT_CursorState cursor_state) {
+			ckit_assert_msg(FALSE, "NOT IMPLEMENTED YET!");
 		}
 	#elif defined(PLATFORM_LINUX)
 		CKIT_Window* ckit_window_create(u32 width, u32 height, const char* name) {
