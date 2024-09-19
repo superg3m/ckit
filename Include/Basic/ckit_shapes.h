a5d2fb76a2d70e7bcc534a77fddcb1bcf63e3020
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-09-19 09:04:25 -0400

    yep

--- Include/Basic/ckit_shapes.h
+++ Include/Basic/ckit_shapes.h
@@ -27,16 +27,7 @@ typedef struct CKIT_Rectangle3D {
 extern "C" {
 #endif
 	CKIT_Rectangle2D ckit_rectangle_create(s32 x, s32 y, u32 width, u32 height);
-	Boolean ckit_rectangle_check_aabb_collision(CKIT_Rectangle2D rect1, CKIT_Rectangle2D rect2);
 
-	/**
-	 * @brief If there is no collision to get then return Zero Initalized CKIT_Rectangle2D 
-	 * 
-	 * @param rect1 
-	 * @param rect2 
-	 * @return CKIT_Rectangle2D 
-	 */
-	CKIT_Rectangle2D ckit_rectangle_get_aabb_collision(CKIT_Rectangle2D rect1, CKIT_Rectangle2D rect2);
 #ifdef __cplusplus
 }
 #endif
@@ -56,19 +47,4 @@ extern "C" {
 		return ret;
 	}
 
-	Boolean ckit_rectangle_check_aabb_collision(CKIT_Rectangle2D rect1, CKIT_Rectangle2D rect2) {	
-		if (rect1.x < rect2.x + rect2.width && rect1.x + rect1.width > rect2.x &&
-			rect1.y < rect2.y + rect2.height && rect1.y + rect1.height > rect2.y) {
-			return TRUE;
-		}
-
-		return FALSE;
-	}
-
-
-	CKIT_Rectangle2D ckit_rectangle_get_aabb_collision(CKIT_Rectangle2D rect1, CKIT_Rectangle2D rect2) {
-		CKIT_Rectangle2D ret = {0};
-
-		return ret;
-	}
 #endif // CKIT_IMPL
