29cdb5695d103a32cbc234a20a084f5f9246fd8b
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-10-03 14:46:59 -0400

    s

--- c_build_script.py
+++ c_build_script.py
@@ -4,6 +4,8 @@ from c_build.source.Manager import *
 # --------------------------------------------------------------------------------------s
 
 compiler_name = C_BUILD_COMPILER_NAME() if C_BUILD_IS_DEPENDENCY() else "cl"
+if !IS_WINDOWS():
+    compiler_name = "gcc"
 compiler_std_version = "c11"
 compiler_warning_level = ""
 compiler_disable_specific_warnings = []
