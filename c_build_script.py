4d1221592ca7aef402a2bae4376fde3439e306bd
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-03-12 22:58:50 -0400

    Update c_build_script.py

--- c_build_script.py
+++ c_build_script.py
@@ -3,65 +3,65 @@ from c_build.source.Utilities import *
 from c_build.source.Manager import *
 # --------------------------------------------------------------------------------------s
 
-compiler_name = C_BUILD_COMPILER_NAME() if C_BUILD_IS_DEPENDENCY() else "INVALID_COMPILER"
+pc: ProjectConfig = ProjectConfig(
+    project_name = "ckit",
+    project_dependencies = ["ckg"],
+    project_debug_with_visual_studio = True,
+    project_rebuild_project_dependencies = False,
+    project_executable_procedures  = ["ckit_test.exe"]
+)
+
+cc: CompilerConfig = CompilerConfig(
+    compiler_name = C_BUILD_COMPILER_NAME() if C_BUILD_IS_DEPENDENCY() else "INVALID_COMPILER",
+    compiler_warning_level = [],
+    compiler_disable_specific_warnings = False,
+    compiler_treat_warnings_as_errors = True,
+    compiler_disable_warnings  = False,
+    compiler_disable_sanitizer = True
+)
 
 if IS_WINDOWS():
-    compiler_name = "cl"
+    cc.compiler_name = "cl"
 if IS_DARWIN():
-    compiler_name = "clang"
+    cc.compiler_name = "clang"
 elif IS_LINUX():
-    compiler_name = "gcc"
-	
-compiler_warning_level = ""
-compiler_disable_specific_warnings = []
-compiler_treat_warnings_as_errors = True
-compiler_disable_warnings = False
-compiler_disable_sanitizer = True
-
-project_name = "ckit"
-project_dependencies = ["ckg"]
-project_rebuild_project_dependencies = False
-project_debug_with_visual_studio = False
-project_executable_procedures = ["ckit_test.exe"]
+    cc.compiler_name = "gcc"
 
 # Do different things depending on the platform
-if compiler_name == "cl":
-	compiler_warning_level = "4"
-	compiler_disable_specific_warnings = ["5105", "4668", "4820", "4996"]
-elif compiler_name in ["gcc", "cc", "clang"]:
-	compiler_warning_level = "all"
-	compiler_disable_specific_warnings = ["deprecated", "parentheses"]
+if cc.compiler_name == "cl":
+	cc.compiler_warning_level = "4"
+	cc.compiler_disable_specific_warnings = ["5105", "4668", "4820", "4996"]
+else:
+	cc.compiler_warning_level = "all"
+	cc.compiler_disable_specific_warnings = ["deprecated", "parentheses"]
 
-executable_procedure_libs = [f"../../build_{compiler_name}/{C_BUILD_LIB('ckit', compiler_name)}"]
-
+executable_procedure_libs = [f"../../build_{cc.compiler_name}/{C_BUILD_LIB('ckit', cc.compiler_name)}"]
 if IS_WINDOWS():
-	windows_libs = ["User32.lib", "Gdi32.lib"] if compiler_name == "cl" else ["-lUser32", "-lGdi32"]
-	executable_procedure_libs += windows_libs
-	
+    windows_libs = ["User32.lib", "Gdi32.lib"] if cc.compiler_name == "cl" else ["-lUser32", "-lGdi32"]
+    executable_procedure_libs += windows_libs
+
 procedures_config = {
-    "ckit_lib": {
-        "build_directory": f"./build_{compiler_name}",
-        "output_name": C_BUILD_LIB('ckit', compiler_name),
-        "source_files": ["../ckg/ckg.c", "../ckit.c"],
-        "additional_libs": [],
-        "compile_time_defines": ["CKIT_WSL"],
-        "compiler_inject_into_args": [],
-        "include_paths": [],
-    },
-    "ckit_core_test": {
-        "build_directory": f"./Tests/build_{compiler_name}",
-        "output_name": "ckit_test.exe",
-        "source_files": ["../*.c"],
-        "additional_libs": executable_procedure_libs,
-        "compile_time_defines": ["CKIT_WSL"],
-        "compiler_inject_into_args": [],
-        "include_paths": [],
-    },
+    "ckit_lib": ProcedureConfigElement(
+        build_directory = f"./build_{compiler_name}",
+        output_name = C_BUILD_LIB('ckit', compiler_name),
+        source_files = ["../ckg/ckg.c", "../ckit.c"],
+        additional_libs = [],
+        compile_time_defines = ["CKIT_WSL"],
+        compiler_inject_into_args = [],
+        include_paths = [],
+    ),
+
+    "ckit_core_test": ProcedureConfigElement(
+        build_directory = f"./Tests/build_{compiler_name}",
+        output_name = "ckit_test.exe",
+        source_files = ["../*.c"],
+        additional_libs = executable_procedure_libs,
+        compile_time_defines = ["CKIT_WSL"],
+        compiler_inject_into_args = [],
+        include_paths = [],
+    ),
 }
 
-compiler_config = {key: value for key, value in locals().items() if key.startswith('compiler_')}
-project_config = {key: value for key, value in locals().items() if key.startswith('project_')}
-
-manager: Manager = Manager(compiler_config, project_config, procedures_config)
+manager: Manager = Manager(cc, pc, procedures_config)
 manager.build_project()
 # -------------------------------------------------------------------------------------
