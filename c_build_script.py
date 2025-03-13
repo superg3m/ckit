6ce8721fdbe826823e92f779d52b333afbcb3e2f
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-03-12 21:10:35 -0400

    Update c_build_script.py

--- c_build_script.py
+++ c_build_script.py
@@ -3,9 +3,13 @@ from c_build.source.Utilities import *
 from c_build.source.Manager import *
 # --------------------------------------------------------------------------------------s
 
-compiler_name = C_BUILD_COMPILER_NAME() if C_BUILD_IS_DEPENDENCY() else "cl"
+compiler_name = C_BUILD_COMPILER_NAME() if C_BUILD_IS_DEPENDENCY() else "INVALID_COMPILER"
 
-if not IS_WINDOWS():
+if IS_WINDOWS():
+    compiler_name = "cl"
+if IS_DARWIN():
+    compiler_name = "clang"
+elif IS_LINUX():
     compiler_name = "gcc"
 	
 compiler_warning_level = ""
