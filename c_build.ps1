222a8e0502183b84f1f951874e8cbcda263d3f43
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-08-03 17:34:08 -0400

    update build system

---
+++ c_build.ps1
@@ -0,0 +1,52 @@
+param (
+    [Parameter(Mandatory = $true)]
+    [ValidateSet("debug", "release")]
+    [string]$BuildType,
+
+    [switch]$Clean,
+    [switch]$Build,
+    [switch]$Debugger,
+    [switch]$Run
+)
+
+$directoryPath = "./c_build"
+$repositoryUrl = "https://github.com/superg3m/c_build.git"
+if (-not (Test-Path -Path $directoryPath)) {
+    Write-Output "Directory does not exist. Cloning the repository..."
+    git clone $repositoryUrl
+} else {
+    Push-Location $directoryPath
+    git fetch origin -q
+    git reset --hard origin/main -q
+    git pull -q
+    Pop-Location
+}
+
+. ./c_build/validate_temp_files.ps1 $MyInvocation.MyCommand.Name
+if (-not ($Build -or $Run -or $Debugger)) {
+    Write-Error "You must specify at least one of -Build, -Run, or -Debugger."
+    exit 1
+}
+
+$BuildType = $BuildType.ToLower()
+
+if ($PSVersionTable.Platform -eq "Unix") {
+    Set-Alias python python3
+}
+
+if ($Clean) {
+    python -B -m c_build_script --execution_type "CLEAN" --build_type $BuildType
+}
+
+if ($Build) {
+    python -B -m c_build_script --execution_type "BUILD" --build_type $BuildType
+}
+
+if ($Debugger) {
+    python -B -m c_build_script --execution_type "BUILD" --build_type "debug"
+    python -B -m c_build_script --execution_type "DEBUG" --build_type "debug"
+}
+
+if ($Run) {
+    python -B -m c_build_script --execution_type "RUN" --build_type $BuildType
+}
